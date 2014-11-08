afe628effdc273a6d3db9122a3b6d3b4cc31404a
arjun <arjunmadan1992@gmail.com>
2014-11-08 14:07:26 -0500

    Text + URL saving complete

--- background.js
+++ background.js
@@ -1,18 +1,21 @@
+var clipboardData = new Array();
+var clipboardURL = new Array();
+var count = 0;
 function saveInfo(info,tab) {
 
-    console.log("Word " + info.selectionText + " was clicked.");
-    chrome.tabs.create({ 
-
-        url: "http://www.google.com/search?q=" + info.selectionText,
-    })
-
-
+	clipboardData[count] = info.selectionText;
+	clipboardURL[count++] = tab.url;
+	for(i = 0; i< count; i++)	
+	{
+		alert(clipboardData[i]);
+		alert(clipboardURL[i]);
+	}
 }
 
 chrome.contextMenus.create({
 
-    title: "Add to clipboard", 
-    contexts:["selection"], 
-    onclick: saveInfo,
+	title: "Add to clipboard", 
+	contexts:["selection"], 
+	onclick: saveInfo
 
 });
